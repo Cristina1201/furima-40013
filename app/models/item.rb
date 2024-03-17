3fe2ed583634e5f0eca5baed2c1de8468ec21102
Cristina1201 <151364864+Cristina1201@users.noreply.github.com>
2024-03-17 18:23:20 +0900

    商品情報登録画面のバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+
+  belongs_to :user
+
+  validates :item_name, presence: true
+  validates :explain, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :delivery_price_id, presence: true
+  validates :place_id, presence: true
+  validates :delivery_day_id, presence: true
+  validates :price, presence: true
+  validates :price, numericality: { in: 300..9999999, only_integer: true }
+  validates :image, presence: true
+
+  
 end
