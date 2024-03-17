356748e499e556ccdbe19bd348b2945b44ae94a4
Cristina1201 <151364864+Cristina1201@users.noreply.github.com>
2024-03-17 18:52:23 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,4 +10,19 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
   
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to '/'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :title, :comment, :category_id, :condition_id, :shipping_fee_burden_id, :prefecture_id, :estimated_shipping_id, :price).merge(user_id: current_user.id)
+  end
+
  end
