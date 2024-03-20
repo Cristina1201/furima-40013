d6bd2e23cc6babe9d48d78f0b4f38ec46baed37a
Cristina1201 <151364864+Cristina1201@users.noreply.github.com>
2024-03-20 22:19:02 +0900

    updateのアクションとルーティングの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,7 +25,20 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    if current_user != @item.user
+      redirect_to root_path
+    end
   end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+  
 
   private
 
